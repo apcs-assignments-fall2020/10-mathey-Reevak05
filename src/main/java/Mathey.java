6b9ee26e3b4a15a65d2f45f933e875d1f38f75d1
Reevak05 <kaveergera@gmail.com>
2020-10-15 18:44:37 +0000

    add Mathey methods

--- src/main/java/Mathey.java
+++ src/main/java/Mathey.java
@@ -1,3 +1,67 @@
 public class Mathey {
 
+    public static int max(int a, int b) {
+        return (a>b) ? a : b;
+    }
+
+    public static double max(double a, double b) {
+        return (a>b) ? a : b;
+    }
+
+    public static int max(int a, int b, int c) {
+        if (a>=b&&a>=c) return a;
+        else if (b>=a&&b>=c) return b;
+        else return c;
+    }
+
+    public static double max(double a, double b, double c) {
+        if (a>=b&&a>=c) return a;
+        else if (b>=a&&b>=c) return b;
+        else return c;
+    }
+
+     public static int min(int a, int b) {
+        return (a<b) ? a : b;
+    }
+
+    public static double min(double a, double b) {
+        return (a<b) ? a : b;
+    }
+
+    public static int min(int a, int b, int c) {
+        if (a<=b&&a<=c) return a;
+        else if (b<=a&&b<=c) return b;
+        else return c;
+    }
+
+    public static double min(double a, double b, double c) {
+        if (a<=b&&a<=c) return a;
+        else if (b<=a&&b<=c) return b;
+        else return c;
+    }
+
+    public static int randomInteger(int a, int b) {
+        if (b<a) return -1;
+        return (int)(Math.random()*(b-a))+a;
+    }
+    public static int randomInteger(int b) {
+        if (b<0) return -1;
+        return (int)(Math.random()*(b));
+    }
+
+    public static void main(String[] args) {
+        //just a bunch of testing
+        System.out.println(max(4, 4));
+        System.out.println(max(5, 5, 4));
+        System.out.println(max(4.3, 6.5));
+        System.out.println(max(0.4, 3.6, 2.3));
+        System.out.println(min(4, 4));
+        System.out.println(min(5, 5, 4));
+        System.out.println(min(4.3, 6.5));
+        System.out.println(min(0.4, 3.6, 2.3));
+        for (int i = 0; i<20; i++) System.out.println(randomInteger(7));
+        for (int i = 0; i<20; i++) System.out.println(randomInteger(3, 9));
+
+    }
+
 }
--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -9,9 +9,7 @@ public class MyMain {
 
     public static void main(String[] args) {
         int[] arr = new int[3];
-        for (int i = 0; i<3;i++) {
-            arr[i] = (int)((Math.random()*11))+10;
-        }
+        for (int i = 0; i<3;i++) arr[i] = (int)((Math.random()*11))+10;
         System.out.println(Arrays.toString(arr));
         System.out.println("largest number: "+Math.max(Math.max(arr[0], arr[1]), arr[2]));
         System.out.println("smallest number: "+Math.min(Math.min(arr[0], arr[1]), arr[2]));
